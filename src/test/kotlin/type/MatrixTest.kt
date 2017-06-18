cda10ed4f54293665f096080fa768f2a880a07b1
Daigo Matsuoka <marcelo.daigo@gmail.com>
2017-06-17 23:35:38 -0300

    Some tests to implement the functions

--- src/test/kotlin/type/MatrixTest.kt
+++ src/test/kotlin/type/MatrixTest.kt
@@ -77,37 +77,91 @@ class MatrixTest {
 
     @Test
     fun subtractMatrixWithSameSize() {
+        val matrix1 = Matrix("5,6:7,8")
+        val matrix2 = Matrix("1,2:3,4")
 
+        val diff = matrix1 - matrix2
+
+        assert(diff[0,0] == 4.0, { "[0,0] should be 4.0 but it was ${diff[0,0]}" })
+        assert(diff[0,1] == 4.0, { "[0,1] should be 4.0 but it was ${diff[0,1]}" })
+        assert(diff[1,0] == 4.0, { "[1,0] should be 4.0 but it was ${diff[1,0]}" })
+        assert(diff[1,1] == 4.0, { "[1,1] should be 4.0 but it was ${diff[1,1]}" })
     }
 
     @Test
     fun subtractMatrixWithDifferentSizes() {
+        val matrix1 = Matrix("1,2:3,4")
+        val matrix2 = Matrix("1:2")
 
+        val exception = shouldThrow<MatrixException> {
+            matrix1 - matrix2
+        }
+        assert(exception.message == Errors.SIZES_SHOULD_MATCH.description)
     }
 
     @Test
     fun associativeMultiplicationMatrixWithSameSize() {
+        val matrix1 = Matrix("5,6:7,8")
+        val matrix2 = Matrix("1,2:3,4")
 
+        val times = matrix1 * matrix2
+
+        assert(times[0,0] == 5.0, { "[0,0] should be 5.0 but it was ${times[0,0]}" })
+        assert(times[0,1] == 12.0, { "[0,1] should be 12.0 but it was ${times[0,1]}" })
+        assert(times[1,0] == 21.0, { "[1,0] should be 21.0 but it was ${times[1,0]}" })
+        assert(times[1,1] == 32.0, { "[1,1] should be 32.0 but it was ${times[1,1]}" })
     }
 
     @Test
     fun associativeMultiplicationMatrixWithDifferentSizes() {
+        val matrix1 = Matrix("1,2:3,4")
+        val matrix2 = Matrix("1:2")
 
+        val exception = shouldThrow<MatrixException> {
+            matrix1 * matrix2
+        }
+        assert(exception.message == Errors.SIZES_SHOULD_MATCH.description)
     }
 
     @Test
     fun scalarMultiplicationMatrix() {
+        val matrix = Matrix("1,2:3,4")
+        val prod = matrix * 2.0
 
+        assert(prod[0,0] == 2.0, { "[0,0] should be 2.0 but it was ${prod[0,0]}" })
+        assert(prod[0,1] == 4.0, { "[0,1] should be 4.0 but it was ${prod[0,1]}" })
+        assert(prod[1,0] == 6.0, { "[1,0] should be 6.0 but it was ${prod[1,0]}" })
+        assert(prod[1,1] == 8.0, { "[1,1] should be 8.0 but it was ${prod[1,1]}" })
     }
 
     @Test
-    fun dotMultiplicationMatrixWithSameSize() {
+    fun dotMultiplicationMatrixWithCorrectDimensions() {
+        val matrix1 = Matrix("1,2:3,4:5,6")
+        val matrix2 = Matrix("7,8,9:10,11,12")
 
+        val dot = matrix1.dot(matrix2)
+
+        assert(dot[0,0] == 27.0, { "[0,0] should be 27.0 but it was ${dot[0,0]}" })
+        assert(dot[0,1] == 30.0, { "[0,1] should be 30.0 but it was ${dot[0,1]}" })
+        assert(dot[0,2] == 33.0, { "[1,0] should be 33.0 but it was ${dot[0,2]}" })
+
+        assert(dot[1,0] == 61.0, { "[0,0] should be 61.0 but it was ${dot[1,0]}" })
+        assert(dot[1,1] == 68.0, { "[0,1] should be 68.0 but it was ${dot[1,1]}" })
+        assert(dot[1,2] == 75.0, { "[1,0] should be 75.0 but it was ${dot[1,2]}" })
+
+        assert(dot[2,0] == 95.0, { "[0,0] should be 95.0 but it was ${dot[2,0]}" })
+        assert(dot[2,1] == 106.0, { "[0,1] should be 106.0 but it was ${dot[2,1]}" })
+        assert(dot[2,2] == 117.0, { "[1,0] should be 117.0 but it was ${dot[2,2]}" })
     }
 
     @Test
-    fun dotMultiplicationMatrixWithDifferentSizes() {
+    fun dotMultiplicationMatrixWithIncorrectDimensions() {
+        val matrix = Matrix("1,2:3,4")
 
+        val exception = shouldThrow<MatrixException> {
+            matrix.dot(matrix)
+        }
+        assert(exception.message == Errors.SHAPES_NOT_ALIGNED.description)
     }
 
     @Test
